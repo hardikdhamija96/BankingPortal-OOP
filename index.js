b1ea37d0fb333e0a9e4783dfafa62091ceb60975
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-09-17 11:59:04 +0530

    Added Index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("test");
+const Account = require("./Account");
+const Customer = require("./Customer");
+const Bank = require("./Bank");
+
+//Create instances
+const bank = new Bank("My Bank");
+const customer1 = new Customer("Amit", "12345");
+const account1 = new Account("001",customer1.customerId,1000);
+
+// Add bank
+bank.addCustomer(customer1);
+bank.addAccount(account1);
+
+// account functionality
+account1.deposit(500);
+account1.withdraw(200);
+
+console.log(customer1.getCustomerInfo());
+console.log(bank.getCustomerAccounts("12345"));
